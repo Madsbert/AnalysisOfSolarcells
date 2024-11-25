6464ec439337a33d46edd937cb59fdd549cef5b9
jakob2rune <jakjes01@easv365.dk>
2024-11-25 19:46:53 +0100

    Some UI changes, Created controller components, and wrote some code for them

--- src/main/java/org/example/analysisofsolarcells/DailyController.java
+++ src/main/java/org/example/analysisofsolarcells/DailyController.java
@@ -1,14 +1,53 @@
 package org.example.analysisofsolarcells;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
 public class DailyController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> SiteIDChoiceBox;
+    private String[] SiteID = {"1234","5678"};
+    private String selectedSiteID="";
+    @FXML
+    private DatePicker DatePicker;
+
+    public void initialize() {
+        //Creating a ChoiceBox for site ID.
+        SiteIDChoiceBox.getItems().addAll(SiteID);
+        //selected Site ID is saved as a String
+        SiteIDChoiceBox.setValue(selectedSiteID);
+    }
+
+    //Get the Date and saves it as String named formattedDate.
+    public void getDate(ActionEvent event) {
+         LocalDate selectedDate = DatePicker.getValue();
+         String formattedDate = selectedDate.format(DateTimeFormatter.ofPattern("dd/MMM/yyyy"));
+         System.out.println(formattedDate);
+    }
+
+    //switches to monthly Scene.
+    public void onMonthlySwitchClick(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(DailyController.class.getResource("Monthly-view.fxml"));
+        Stage stage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 667, 525);
+        stage.setTitle("login Screen");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void onCompareGraphClick(ActionEvent actionEvent) {
+        //if time, create a method the clears choiceboxes, datepicker and results, and allows a new graph.
     }
 }
--- src/main/java/org/example/analysisofsolarcells/MonthlyController.java
+++ src/main/java/org/example/analysisofsolarcells/MonthlyController.java
@@ -1,4 +1,72 @@
 package org.example.analysisofsolarcells;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class MonthlyController {
+
+    @FXML
+    private Spinner<Integer> YearSpinner;
+    private int currentYear=0;
+    @FXML
+    private ChoiceBox<String> MonthChoiceBox;
+    private String[] Months = {
+            "January", "February", "March", "April", "May", "June","July", "August", "September", "October","November","December"};
+    private String selectedMonth="";
+
+    @FXML
+    private ChoiceBox<String> SiteIDChoiceBox;
+    private String[] SiteID = {"1234","5678"};
+    private String selectedSiteID="";
+
+
+
+    public void initialize() {
+        //spinner value factory object
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1990, 2100);
+        //start value
+        valueFactory.setValue(2024);
+        //inserts start value
+        YearSpinner.setValueFactory(valueFactory);
+        //sets the value to currentWeight variable, for calculation
+        currentYear = YearSpinner.getValue();
+
+
+        //Creating ChoiceBoxes
+        MonthChoiceBox.getItems().addAll(Months);
+        //selected month is saved as a String.
+        selectedMonth = MonthChoiceBox.getValue();
+        SiteIDChoiceBox.getItems().addAll(SiteID);
+        //selected Site ID is saved as a String
+        SiteIDChoiceBox.setValue(selectedSiteID);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public void onDailySwitchClick(ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MonthlyController.class.getResource("Daily-view.fxml"));
+        Stage stage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 667, 525);
+        stage.setTitle("login Screen");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
